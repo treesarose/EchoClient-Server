27899e5fa2f0d807a9be424d7882dab857e1e40d
treesarose <36188557+treesarose@users.noreply.github.com>
2018-04-04 10:41:47 +0530

    Update EchoServer.java

--- EchoServer.java
+++ EchoServer.java
@@ -1,17 +1,14 @@
 import java.io.*;
 import java.net.*;
 
-public class EchoServer
-{
-	public EchoServer(int portnum)
-	{
+public class EchoServer{
+	public EchoServer(int portnum){
 		try{
 			server = new ServerSocket(portnum);
 		}catch (Exception err){
 			System.out.println(err);
 		}
 	}
-
 	public void serve(){
 		try{
 			while (true){
